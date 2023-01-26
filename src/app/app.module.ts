d8ce36d854f3e033a568e0ab9beba3abc753d741
Darryl Ng <darryl1975@hotmail.com>
2023-01-26 12:12:19 +0800

    templated form 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { RouterModule } from '@angular/router';
+import { AnimalFormComponent } from './animal-form/animal-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemListComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    AnimalFormComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +23,7 @@ import { RouterModule } from '@angular/router';
       {path: 'employeelist', component: EmployeeListComponent},
     ]),
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
